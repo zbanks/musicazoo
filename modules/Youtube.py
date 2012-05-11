2bef765cf899f4feead3c9898b0669a44d758e3d
Chris Merrill <cmerrill@mit.edu>
2012-05-11 15:28:22 -0400

    Refactored youtube to not use playyoutube.sh. Audio-only now (should) work.
    This needs to be tested... but I sadly can't because musicazoo has
    the things I need. *sigh*

--- modules/Youtube.py
+++ modules/Youtube.py
@@ -4,8 +4,10 @@ from subprocess import Popen, PIPE
 import gdata.youtube.service as yt
 import re
 import urlparse
+import os
 
 null_f = open("/dev/null", "rw")
+cookie_file="/var/tmp/youtube-dl-cookies.txt"
 
 def getYouTubeIdFromUrl(url):
     try:
@@ -19,8 +21,9 @@ class Youtube(MusicazooShellCommandModule):
     resources = ("audio", "screen")
     persistent = False
     keywords = ("youtube", "yt")
-    command = ("/usr/local/bin/playyoutube",)
     duration = 0
+    command = ("mplayer","-slave","-framedrop","-cache","8192","-vo","xv",
+               "-fs","-cookies","-cookies-file",cookie_file)
 
     @staticmethod
     def match(input_str):
@@ -32,8 +35,6 @@ class Youtube(MusicazooShellCommandModule):
 
         # Initialize module parameters
         self.url = json["arg"].strip().split(" ")[-1]
-        self.command += (self.url,)
-
         self.status_dict = {"url": self.url}
 
         # Get title of youtube video
@@ -55,14 +56,18 @@ class Youtube(MusicazooShellCommandModule):
                 self.subprocess.stdin.write(json["command"]+"\n")
 
     def _run(self,cb):
-        command = self.command
-        self.subprocess = Popen(command, stderr=null_f, stdout=PIPE, stdin=PIPE)
+        youtube_dl = Popen(("youtube-dl","-g","--max-quality=18","--cookies", cookie_file, self.url),
+                           stderr=null_f, stdin=null_f, stdout=PIPE)
+        dl_cmd = self.command + (youtube_dl.stdout.readline(),)
+        
+        mplayer_env = os.environ.copy()
+        mplayer_env["DISPLAY"]=":0"
+        mplayer = Popen(dl_cmd, stderr=null_f, stdout=PIPE, stdin=PIPE, env=mplayer_env)
+        self.subprocess = mplayer
         print "Running youtube"
 
-
         button_template = "<a class='rm button' href='/msg?for_id=%s&command=%%s'>%%s</a>" % self.id
-        button_list = {"pause" : "pause",
-                      }
+        button_list = {"pause" : "pause",}
         buttons = "\n".join(map(lambda x: button_template % x, button_list.items()))
         self.timein = "0:00"
         self.seconds = 0
--- modules/YoutubeAudio.py
+++ modules/YoutubeAudio.py
@@ -1,12 +1,15 @@
 from Youtube import Youtube
 import re
 
+cookie_file="/var/tmp/youtube-dl-cookies.txt"
+
 # This class plays /only/ the audio from a youtube video
 # Note -- Not currently sure if this works yet
 class YoutubeAudio(Youtube):
     keywords = ("ytaudio",)
     resources = ("audio",)
-    command = ("/usr/local/bin/playyoutube","-vo","null")
-    
+    command = ("mplayer","-slave","-framedrop","-cache","8192","-vo","null",
+               "-fs","-cookies","-cookies-file",cookie_file)
+   
     def match(input_str):
         return False
