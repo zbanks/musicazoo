28b41a6161c3f66965a0fa9702362408fff1ee8f
Eric Van Albert <eric@ericslaptop.(none)>
2013-07-04 04:18:28 -0700

    Adding vol.py

---
+++ vol.py
@@ -0,0 +1,16 @@
+class VolumeModule:
+	TYPE_STRING="volume"
+
+	def __init__(self):
+		self.vol=50
+
+	def get_vol(self):
+		return self.vol
+
+	def set_vol(self,vol):
+		self.vol=vol
+
+	validCommands={
+		'get_vol':get_vol,
+		'set_vol':set_vol
+	}
