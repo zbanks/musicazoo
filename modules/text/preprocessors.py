888da09d1781c19c115b4c6108bf3d0a9fa3d93d
Zach Banks <zbanks@mit.edu>
2013-07-08 01:27:51 -0700

    Text preprocessing

--- modules/text/preprocessors.py
+++ modules/text/preprocessors.py
@@ -6,10 +6,10 @@ def no_preprocessor(text):
 def casual(text):
     text = re.sub(r'(?:\w*://)?(?:.*?\.)?(?:([a-zA-Z-1-9]*)\.)?([a-zA-Z-1-9]*\.[a-zA-Z]{1,})', " link ", text)
     subs = (
+        (r'mit[.]edu', " mit dot edju "),
         (r'(\w+://)?(.*?\.)?(([a-zA-Z-1-9]*)\.)?([a-zA-Z-1-9]*\.[a-zA-Z]{1,})', " link "),
         (r'<3', " wub "),
-        (r'zbanks', " z banks "),
-        (r'mit[.]edu', " mit dot edju ")
+        (r'zbanks', " z banks ")
     )
     for reg, repl in subs:
         text = re.sub(reg, repl, text, re.IGNORECASE)
