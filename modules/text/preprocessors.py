e34f3232007924f4db04f22ac3d26fa81aa12c6e
Eric Van Albert <eric@ericslaptop.(none)>
2013-07-08 01:46:39 -0700

    Reading room numbers and links

--- modules/text/preprocessors.py
+++ modules/text/preprocessors.py
@@ -7,7 +7,9 @@ def pronounciation(text):
     subs = (
         (r'mit\.edu', " mit dot edju "),
         (r'<3', " wub "),
-        (r'zbanks', " z banks ")
+        (r'zbanks', " z banks "),
+        (r'(\d{1,2})-(\d{1,2})00',r'\1 \2 hundred'),
+        (r'(\d{1,2})-(\d{1,2})(\d\d)',r'\1 \2 \3'),
     )
     text = remove_urls(text)
     for reg, repl in subs:
@@ -16,9 +18,9 @@ def pronounciation(text):
 
 def remove_urls(text):
 	text=re.sub(r'[A-Za-z]+://[^ ]+',r'link',text)
-	text=re.sub(r'www\.[^ ]+\.[^ ]+',r'link',text)
+	text=re.sub(r'[^ ]+\.[^ ]+/[^ ]*',r'link',text)
 	return text
 
 
 if __name__=='__main__':
-	print remove_urls('This is a link: www.google.com')
+	print pronounciation('Reuse in 1-290 and 13-1800 google.com www.google.com www.google.com/ http://google.com')
