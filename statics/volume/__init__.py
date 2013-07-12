9efbb234df4ada672a82c35146bd4a4ed1667ba1
Zach Banks <zach@chartio.com>
2013-07-11 18:58:08 -0700

    Volume static to use alsaaudio/osax

--- statics/volume/__init__.py
+++ statics/volume/__init__.py
@@ -1,23 +1,46 @@
-import alsaaudio
+try:
+    import alsaaudio
+except:
+    alsaaudio = None
+try:
+    import osax
+except:
+    osax = None
+
 
 class Volume:
-	def __init__(self):
-		self.mixer=alsaaudio.Mixer()
+    def __init__(self):
+        if alsaaudio:
+            self.mixer=alsaaudio.Mixer()
+        elif osax:
+            self.mixer = osax.OSAX()
+        else:
+            print "Unable to control volume"
 
-	def get_vol(self):
-		return int(self.mixer.getvolume()[0])
+    def get_vol(self):
+        if alsaaudio:
+            return int(self.mixer.getvolume()[0])
+        elif osax:
+            return self.mixer.get_volume_settings()[osax.k.output_volume]
+        else:
+            return 0
 
-	def set_vol(self,vol):
-		self.mixer.setvolume(vol)
+    def set_vol(self,vol):
+        if alsaaudio:
+            self.mixer.setvolume(vol)
+        elif osax:
+            self.mixer.set_volume(output_volume=vol)
+        else:
+            pass
 
-	commands={
-		'set_vol':set_vol
-	}
+    commands={
+        'set_vol':set_vol
+    }
 
-	parameters={
-		'vol':get_vol
-	}
+    parameters={
+        'vol':get_vol
+    }
 
-	constants={
-		'class':'volume'
-	}
+    constants={
+        'class':'volume'
+    }
