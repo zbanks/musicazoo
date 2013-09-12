c193b1195bda574173156dea15fe184031f781d9
Zach Banks <zbanks@mit.edu>
2013-09-12 02:50:28 -0400

    Stubby code for @ervanalb

--- musicazoo/bots/mzbot.py
+++ musicazoo/bots/mzbot.py
@@ -4,30 +4,44 @@ import requests
 import json
 
 class MZBot(object):
-	def __init__(self, endpoint):
-		self.endpoint = endpoint
+    def __init__(self, endpoint):
+        self.endpoint = endpoint
+        self.module_capabilities = None
+        self.static_capabilities = None
+        self.background_capabilities = None
 
-	def doCommands(self, cmd_list):
-		json_data = json.dumps(cmd_list)
-		headers = {"Content-type": "text/json"}
-		resp = requests.post(self.endpoint, data=json_data, headers=headers)
-		return resp.json()
+    def doCommands(self, cmd_list):
+        json_data = json.dumps(cmd_list)
+        headers = {"Content-type": "text/json"}
+        resp = requests.post(self.endpoint, data=json_data, headers=headers)
+        print resp.json()
+        return resp.json()
+    
+    def fetch_capabilities(self):
+        cap_resp = self.assert_success(self.doCommands([
+            {"cmd":"module_capabilities"},
+            {"cmd":"static_capabilities"},
+            {"cmd":"background_capabilities"}
+        ]))
+        self.module_capabilities = cap_resp[0]
+        self.static_capabilities = cap_resp[1]
+        self.background_capabilities = cap_resp[2]
 
-	def assert_success(self,result):
-		if isinstance(result,dict):
-			if result['success']:
-				return
-			raise Exception(result['error'])
-		
-		if isinstance(result,list):
-			for r in result:
-				if not r['success']:
-					raise Exception(r['error'])
-				return
-		raise Exception('Bad response type from server')
+    def assert_success(self,result):
+        if isinstance(result,dict):
+            if result['success']:
+                return result['result']
+            raise Exception(result['error'])
+        
+        if isinstance(result,list):
+            for r in result:
+                if not r['success']:
+                    raise Exception(r['error'])
+            return [r.get('result') for r in result]
+        raise Exception('Bad response type from server')
 
 if __name__=='__main__':
-	import sys
-	endpoint = sys.argv[1] if len(sys.argv) > 1 else "http://localhost:9000/"
-	b = MZBot(endpoint)
-	print b.doCommands([{'cmd':'static_capabilities'}])
+    import sys
+    endpoint = sys.argv[1] if len(sys.argv) > 1 else "http://localhost:9000/"
+    b = MZBot(endpoint)
+    print b.doCommands([{'cmd':'static_capabilities'}])
