14df461febcab5a4068faf17a1ae2666ce5785f8
Zach Banks <zbanks@mit.edu>
2013-12-07 06:42:41 -0500

    Top viewer supports JSON response

--- musicazoo/bots/top/viewer.py
+++ musicazoo/bots/top/viewer.py
@@ -29,6 +29,9 @@ class ViewerBot(Webserver):
 	def __init__(self):
 		Webserver.__init__(self,HOST_NAME,PORT_NUMBER)
 
+	def json_transaction(self, json):
+		return self.make_json_list(limit=30)
+
 	def html_transaction(self,form_data):
 		try:
 			n=int(form_data['n'])
@@ -53,6 +56,10 @@ class ViewerBot(Webserver):
 
 	def makelist(self,n=30):
 		global yt_cache
+		def format_yt_link(url, title=None):
+			if title is not None:
+				return u"<a href='{0}'>{1}</a>".format(url, title)
+			return u"<a href='{0}'>{0}</a>".format(url, url)
 		self.db=os.path.join(os.path.dirname(os.path.realpath(__file__)),'mz.db')
 		out=u''
 		conn=sqlite3.connect(self.db)
@@ -70,9 +77,9 @@ class ViewerBot(Webserver):
 				info=get_info(vid)
 				url="http://youtube.com/watch?v={0}".format(vid)
 				if 'title' in info:
-					yt_cache[vid]=u'<a href="{0}">{1}</a>'.format(url,info['title'])
+					yt_cache[vid]=(url,info['title'])
 				else:
-					yt_cache[vid]=u'<a href="{0}">{0}</a>'.format(url)
+					yt_cache[vid]=(url)
 
 			if count==last_count:
 				n_to_show==last_n
@@ -81,10 +88,48 @@ class ViewerBot(Webserver):
 				last_count=count
 			n+=1
 
-			out+=u'\t<p>{0}. {1} ({2})</p>\n'.format(n_to_show,yt_cache[vid],count)
+			out+=u'\t<p>{0}. {1} ({2})</p>\n'.format(n_to_show,format_yt_link(*yt_cache[vid]),count)
 		conn.commit()
 		conn.close()
 		return out
+
+	def make_json_list(self, limit=30):
+		global yt_cache
+		self.db=os.path.join(os.path.dirname(os.path.realpath(__file__)),'mz.db')
+		out=u''
+		conn=sqlite3.connect(self.db)
+		c=conn.cursor()
+		c.execute('SELECT COUNT(*) as num,url FROM youtube_history GROUP BY url ORDER BY num DESC, t DESC LIMIT {0}'.format(limit))
+		n=1
+		last_n=1
+		last_count=None
+		result = []
+		while True:
+			line=c.fetchone()
+			if not line:
+				break
+			(count,vid)=line
+			if vid not in yt_cache:
+				info=get_info(vid)
+				url="http://youtube.com/watch?v={0}".format(vid)
+				if 'title' in info:
+					yt_cache[vid]=(url,info['title'])
+				else:
+					yt_cache[vid]=(url)
+
+			if count==last_count:
+				n_to_show==last_n
+			else:
+				n_to_show=n
+				last_count=count
+			n+=1
+
+			result.append({"n": n_to_show, "url": yt_cache[vid][0], "title": yt_cache[vid][1], "count": count})
+
+#out+=u'\t<p>{0}. {1} ({2})</p>\n'.format(n_to_show,yt_cache[vid],count)
+		conn.commit()
+		conn.close()
+		return result
 
 if __name__=='__main__':
 	ViewerBot().run()
