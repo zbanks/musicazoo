7ca2a27c0aec6dd945ac6dce6d35e915c8ccd80c
Zach Banks <zbanks@mit.edu>
2014-11-29 21:45:42 -0500

    I have no idea what I'm doing

--- musicazoo/queue/modules/youtube.py
+++ musicazoo/queue/modules/youtube.py
@@ -10,20 +10,108 @@ if __name__ != '__main__':
 else:
     # If name is main then this is the sub-process
     # admittedly, it does feel very fork-esque
+    import os
+    import tempfile
+    import time
+    import youtube_dl 
+
     import musicazoo.lib.packet as packet
     import musicazoo.queue.pymodule as pymodule
 
-    conn=pymodule.ParentConnection()
+    from youtube_dl.compat import compat_cookiejar, compat_urllib_request
+    from youtube_dl.utils import make_HTTPS_handler, YoutubeDLHandler
+    from musicazoo.lib.vlc_player_compat import Player
 
-    def poller():
-        while True:
-            print "MOD RECV",conn.recv_cmd()
-            conn.send_resp(packet.good())
+    from musicazoo.lib.watch_dl import WatchCartoonOnlineIE
+
+    class YoutubeModule(pymodule.JSONParentPoller):
+        def cmd_init(self, url):
+            print "URL:", url
+            self.player=Player()
+            self.url=url
+            self.title=None
+            self.duration=None
+            self.site=None
+            self.media=None
+            self.thumbnail=None
+            self.description=None
+            self.time=None
+            self.vid=None
+            self.cookies=None
+            self.rate=None
+            self.status='added'
+            self.getVideoInfo()
+            return packet.good()
+
+        def cmd_play(self):
+            print "Play"
+            self.player.load(self.media,cookies=self.cookies)
+            return packet.good()
+
+        def cmd_suspend(self):
+            print "Suspend"
+            self.player.stop()
+
+        def getVideoInfo(self):
+            url = self.url
+            # General configuration
+            tf=tempfile.NamedTemporaryFile(delete=False)
+            tf.close()
+            self.cookies=tf.name
+            jar = compat_cookiejar.MozillaCookieJar(self.cookies)
+            cookie_processor = compat_urllib_request.HTTPCookieProcessor(jar)
+            proxies = compat_urllib_request.getproxies()
+            proxy_handler = compat_urllib_request.ProxyHandler(proxies)
+            https_handler = make_HTTPS_handler(None)
+            opener = compat_urllib_request.build_opener(https_handler, proxy_handler, cookie_processor, YoutubeDLHandler)
+            compat_urllib_request.install_opener(opener)
+
+            y=youtube_dl.YoutubeDL({'outtmpl':u'','skip_download':True}) # empty outtmpl needed due to weird issue in youtube-dl
+            y.add_info_extractor(WatchCartoonOnlineIE())
+            y.add_default_info_extractors()
+
+            try:
+                info=y.extract_info(url,download=False)
+            except Exception:
+                raise
+                self.status='invalid'
+                self.queue.removeMeAsync(self.uid) # Remove if possible
+                self.ready.release()
+                return False
+
+            jar.save()
+
+            if 'entries' in info:
+                vinfo=info['entries'][0]
+            else:
+                vinfo=info
+
+            if 'title' in vinfo:
+                self.title=vinfo['title']
+            if 'duration' in vinfo:
+                self.duration=vinfo['duration']
+            if 'extractor' in vinfo:
+                self.site=vinfo['extractor']
+            if 'url' in vinfo:
+                self.media=vinfo['url']
+            if 'thumbnail' in vinfo:
+                self.thumbnail=vinfo['thumbnail']
+            if 'description' in vinfo:
+                self.description=vinfo['description']
+            if 'id' in vinfo:
+                self.vid=vinfo['id']
+
+            if self.status=='added':
+                self.status='ready'
+
+            return True
+
+    mod = YoutubeModule()
 
     import sys
     import threading
 
-    t=threading.Thread(target=poller)
+    t=threading.Thread(target=mod.poller)
     t.daemon=True
     t.start()
 
@@ -32,4 +120,4 @@ else:
         time.sleep(1)
     
     print "QUITTING"
-    conn.close()
+    mod._close()
--- musicazoo/queue/pymodule.py
+++ musicazoo/queue/pymodule.py
@@ -1,16 +1,20 @@
+import json
 import socket
 import sys
-import json
+import traceback
+
+import musicazoo.lib.packet as packet
 
 # Modules can be written in any language, but if you choose to write them in python.
 # you may find the contents of this file helpful.
 
 # Connects back to the queue based on command-line arguments
-class ParentConnection():
+class ParentConnection(object):
     def __init__(self):
         host = sys.argv[1]
         cmd_port = int(sys.argv[2])
         update_port = int(sys.argv[3])
+        print "New Connection:", host, cmd_port, update_port
         self.cs=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.us=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.cs.connect((host,cmd_port))
@@ -39,3 +43,31 @@ class ParentConnection():
     def close(self):
         self.cs.close()
         self.us.close()
+
+class JSONParentPoller(object):
+    def __init__(self):
+        self.connection = ParentConnection()
+
+    def close(self):
+        return self.connection.close()
+
+    def poller(self):
+        while True:
+            data = self.connection.recv_cmd()
+            print "Module Recieved: ", data
+            cmd = "cmd_" + data.get("cmd")
+
+            if not hasattr(self, cmd):
+                response = packet.error("Invalid command")
+            else:
+                args = data.get("args", {})
+
+                try:
+                    response = getattr(self, cmd)(**args)
+                except Exception:
+                    traceback.print_exc()
+                    response = packet.error("Generic multi-command processing error")
+            
+            if response is not None:
+                self.connection.send_resp(response)
+
