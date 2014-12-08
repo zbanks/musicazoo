de1f32c4cef3bcf9a89f2fed6778a4229de27ba0
Zach Banks <zbanks@mit.edu>
2014-12-08 01:59:46 -0500

    Packaging! python setup.py install ftw

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Musicazoo',
+    version='5.0',
+    description='Modular media player',
+    author='Zach Banks',
+    author_email='zbanks@mit.edu',
+    url='https://github.com/zbanks/musicazoo',
+    packages=['musicazoo', 'musicazoo.wsgi', 'musicazoo.queue', 'musicazoo.volume', 'musicazoo.lib'],
+    scripts=[
+        "bin/musicazoo", "bin/mz"
+    ],
+    package_dir = {
+        'musicazoo.wsgi': 'musicazoo/wsgi'
+    },
+    package_data={
+        'musicazoo.wsgi': ['static/*.html', 'static/assets/*.js', 'static/assets/*.otf', 'static/assets/*.css', 'static/assets/images/*']
+    }
+)
