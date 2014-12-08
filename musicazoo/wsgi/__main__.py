004bd4896ae35691863e3c0c89f562df2bd08873
Zach Banks <zbanks@mit.edu>
2014-12-08 00:59:07 -0500

    Fixing minor deploy issues

--- musicazoo/wsgi/__main__.py
+++ musicazoo/wsgi/__main__.py
@@ -1,4 +1,4 @@
 import musicazoo.wsgi
 import werkzeug.serving
 
-werkzeug.serving.run_simple('localhost',8080,musicazoo.wsgi.application,extra_files='settings.py')
+werkzeug.serving.run_simple('',8080,musicazoo.wsgi.application,extra_files='settings.py')
