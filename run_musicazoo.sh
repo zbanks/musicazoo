2398a41e3569ca845fcf8626af9e8d3576404c91
Zach Banks <zbanks@mit.edu>
2014-12-08 03:48:05 -0500

    Fixed static files and supervisord.conf

--- run_musicazoo.sh
+++ run_musicazoo.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-export PYTHONPATH=`pwd`
-supervisord -c supervisord.conf
+#export PYTHONPATH=`pwd`
+
+#SCONF="supervisord.conf"
+SCONF=$(python -c "import pkg_resources; print pkg_resources.resource_filename('musicazoo', '../supervisord.conf')")
+
+supervisord -c $SCONF
