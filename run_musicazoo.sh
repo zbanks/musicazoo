602b2609b57692f67cb1af4f5653b1bd9f96e742
Zach Banks <zjbanks@gmail.com>
2017-02-08 19:16:52 -0500

    Fix run_musicazoo.sh to detect SHMOOZE_SETTINGS by default; bump to 5.2.6

--- run_musicazoo.sh
+++ run_musicazoo.sh
@@ -2,14 +2,16 @@
 
 #export PYTHONPATH=`pwd`
 
-export SHMOOZE_SETTINGS=$1
+export SHMOOZE_SETTINGS="$1"
+if [ $# -eq 0 ]; then
+    export SHMOOZE_SETTINGS=$(python -c "import pkg_resources; print pkg_resources.resource_filename('musicazoo', '../settings.json')") 
+fi
 
 # Workaround VLC bug
 export VDPAU_DRIVER=va_gl
 
 #SCONF="supervisord.conf"
 SCONF=$(python -c "import pkg_resources; print pkg_resources.resource_filename('musicazoo', '../supervisord.conf')")
-#SETTINGS=$(python -c "import pkg_resources; print pkg_resources.resource_filename('musicazoo', '../settings.json')") 
 
 echo "Supervisor configuration: $SCONF";
 echo "Musicazoo settings.json:  $SHMOOZE_SETTINGS";
